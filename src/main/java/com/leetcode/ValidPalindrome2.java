9961721dc9fdce0d3e2436897a170d92b6753139
Carl <zxfspace@163.com>
2019-08-19 05:02:28 +0800

    valid palindrome 2

---
+++ src/main/java/com/leetcode/ValidPalindrome2.java
@@ -0,0 +1,45 @@
+package com.leetcode;
+
+/**
+ * https://leetcode.com/problems/valid-palindrome-ii/
+ * <p>
+ * Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+ * <p>
+ * Example 1:
+ * Input: "aba"
+ * Output: True
+ * Example 2:
+ * Input: "abca"
+ * Output: True
+ * Explanation: You could delete the character 'c'.
+ * Note:
+ * The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+ *
+ * @author carl
+ */
+public class ValidPalindrome2 {
+
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) == s.charAt(r)) {
+                l++;
+                r--;
+            } else {
+                return process(s, l, r - 1) || process(s, l + 1, r);
+            }
+        }
+        return true;
+    }
+
+    private boolean process(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
