f5ba6985ae93880d7852d0bb521ee6766dcd5006
Carl <zxfspace@163.com>
2019-08-19 19:26:34 +0800

    regular expression matching

---
+++ src/test/java/com/data/structure/leetcode/RegularExpressionMatchingTest.java
@@ -0,0 +1,29 @@
+package com.data.structure.leetcode;
+
+import com.leetcode.RegularExpressionMatching;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author carl
+ */
+public class RegularExpressionMatchingTest {
+
+    @Test
+    public void test() {
+        RegularExpressionMatching matcher = new RegularExpressionMatching();
+
+        assertTrue(matcher.isMatch("", ""));
+        assertTrue(matcher.isMatch("a", "."));
+        assertTrue(matcher.isMatch("ab", ".*"));
+        assertTrue(matcher.isMatch("aa", "a*"));
+        assertTrue(matcher.isMatch("aaa", "a*a"));
+        assertTrue(matcher.isMatch("aaa", "a*aaa"));
+        assertTrue(matcher.isMatch("aab", "c*a*b"));
+        assertFalse(matcher.isMatch("aa", "a"));
+        assertFalse(matcher.isMatch("mississippi", "mis*is*p*."));
+        assertFalse(matcher.isMatch("ab", ".*c"));
+    }
+}
