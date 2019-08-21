ab7447d215d548df67790b7f77c23225b7a80fa0
Carl <zxfspace@163.com>
2019-08-21 21:40:58 +0800

    longest common prefix

--- src/main/java/com/leetcode/LongestCommonPrefix.java
+++ src/main/java/com/leetcode/LongestCommonPrefix.java
@@ -23,57 +23,25 @@ package com.leetcode;
  * @author carl
  */
 public class LongestCommonPrefix {
-    public static final int SIZE = 26;
-
-    Node root;
-    StringBuilder sb;
 
     public String longestCommonPrefix(String[] strs) {
-        root = new Node(' ', null);
-        sb = new StringBuilder();
-        Node p;
-        int i;
-        for (i = 0; i < strs.length - 1; i++) {
-            p = root;
-            for (int j = 0; j < strs[i].length(); j++) {
-                append(p, i, j, strs);
-                p = p.children[strs[i].charAt(j) - 'a'];
-            }
+        if (strs.length == 0) {
+            return "";
         }
-        if (i == strs.length - 1) {
-            p = root;
-            for (int j = 0; j < strs[i].length(); j++) {
-                append(p, i, j, strs);
-                if (p.children[strs[i].charAt(j) - 'a'].count == strs.length) {
-                    sb.append(strs[i].charAt(j));
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            for (int j = 0; j < strs.length; j++) {
+                if (strs[j].length() == 0 || i == strs[j].length()) {
+                    return sb.toString();
                 }
-                p = p.children[strs[i].charAt(j) - 'a'];
+                if (j > 0 && strs[j].charAt(i) != strs[j - 1].charAt(i)) {
+                    return sb.toString();
+                }
             }
+            sb.append(strs[0].charAt(i));
         }
-
         return sb.toString();
-    }
-
-    private void append(Node p, int i, int j, String[] strs) {
-        if (p.children[strs[i].charAt(j) - 'a'] == null) {
-            Node node = new Node(strs[i].charAt(j), p);
-            p.children[strs[i].charAt(j) - 'a'] = node;
-        } else {
-            p.children[strs[i].charAt(j) - 'a'].count++;
-        }
-    }
-
-    static class Node {
-        public char value;
-        public int count;
-        public Node[] children;
-        public Node pre;
-
-        public Node(char c, Node pre) {
-            this.value = c;
-            this.count = 1;
-            this.children = new Node[SIZE];
-            this.pre = pre;
-        }
     }
 }
