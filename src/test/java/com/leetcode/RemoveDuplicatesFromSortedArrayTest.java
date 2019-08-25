d6bef785390555adadbc48de61339967e65b3899
Carl <zxfspace@163.com>
2019-08-25 11:04:26 +0800

    remove duplicates from sorted array

---
+++ src/test/java/com/leetcode/RemoveDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,31 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author carl
+ */
+public class RemoveDuplicatesFromSortedArrayTest {
+
+    @Test
+    public void test() {
+        RemoveDuplicatesFromSortedArray rem = new RemoveDuplicatesFromSortedArray();
+
+        int[] expected = new int[]{1, 2};
+        int[] a = new int[]{1, 1, 2};
+        int len = rem.removeDuplicates(a);
+
+        assertEquals(expected.length, len);
+        assertTrue(() -> {
+            for (int i = 0; i < expected.length; i++) {
+                if (expected[i] != a[i]) {
+                    return false;
+                }
+            }
+            return true;
+        });
+    }
+}
