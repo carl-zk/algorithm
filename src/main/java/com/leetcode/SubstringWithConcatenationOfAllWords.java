a7a12d95d2651a7304f6fd1443ab48b9441fdc9c
Carl <zxfspace@163.com>
2019-08-26 20:18:26 +0800

    优化滑动窗口到最佳

--- src/main/java/com/leetcode/SubstringWithConcatenationOfAllWords.java
+++ src/main/java/com/leetcode/SubstringWithConcatenationOfAllWords.java
@@ -7,6 +7,27 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * 时间复杂度O(n)
+ * 6ms, 超过96.25%
+ * https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+ * <p>
+ * You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * s = "barfoothefoobarman",
+ * words = ["foo","bar"]
+ * Output: [0,9]
+ * Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar" respectively.
+ * The output order does not matter, returning [9,0] is fine too.
+ * Example 2:
+ * <p>
+ * Input:
+ * s = "wordgoodgoodgoodbestword",
+ * words = ["word","good","best","word"]
+ * Output: []
+ *
  * @author carl
  */
 public class SubstringWithConcatenationOfAllWords {
