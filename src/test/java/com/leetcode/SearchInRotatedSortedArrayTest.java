cf182d365c212aa9187989d4d0cc9d8857226801
Carl <zxfspace@163.com>
2019-08-28 00:49:20 +0800

    search in rotated sorted array

---
+++ src/test/java/com/leetcode/SearchInRotatedSortedArrayTest.java
@@ -0,0 +1,23 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author carl
+ */
+public class SearchInRotatedSortedArrayTest {
+
+    @Test
+    public void test() {
+        SearchInRotatedSortedArray search = new SearchInRotatedSortedArray();
+        int expect = 4;
+        int result = search.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0);
+        assertEquals(expect, result);
+
+        expect = -1;
+        result = search.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3);
+        assertEquals(expect, result);
+    }
+}
