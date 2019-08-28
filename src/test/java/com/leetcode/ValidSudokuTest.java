03d64aa1aa58aad0c1a14dc8a81987120c80320a
Carl <zxfspace@163.com>
2019-08-29 00:15:07 +0800

    valid sudoku

---
+++ src/test/java/com/leetcode/ValidSudokuTest.java
@@ -0,0 +1,49 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author carl
+ */
+public class ValidSudokuTest {
+    ValidSudoku validSudoku = new ValidSudoku();
+
+    @Test
+    public void test() {
+        char[][] input = new char[][]{
+                new char[]{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                new char[]{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        boolean result = validSudoku.isValidSudoku(input);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testInvalid() {
+        char[][] input = new char[][]{
+                new char[]{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                new char[]{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        boolean result = validSudoku.isValidSudoku(input);
+        assertFalse(result);
+    }
+}
