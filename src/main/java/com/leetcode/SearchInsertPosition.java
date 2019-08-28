abb7800dd78e152c2c202da272050c5e99bae574
carl <xinfeng.zhao2@wework.com>
2019-08-28 18:18:38 +0800

    search insert position(not yet)

---
+++ src/main/java/com/leetcode/SearchInsertPosition.java
@@ -0,0 +1,11 @@
+package com.leetcode;
+
+/**
+ * @author carl
+ */
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/leetcode/SearchInsertPositionTest.java
@@ -0,0 +1,33 @@
+package com.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+/**
+ * @author carl
+ */
+public class SearchInsertPositionTest {
+    SearchInsertPosition search = new SearchInsertPosition();
+
+    @ParameterizedTest
+    @MethodSource("source")
+    public void test(int[] nums, int target, int expect) {
+        int result = search.searchInsert(nums, target);
+        assertEquals(expect, result);
+    }
+
+    static Stream<Arguments> source() {
+        return Stream.of(
+                arguments(new int[]{1, 3, 5, 6}, 5, 2),
+                arguments(new int[]{1, 3, 5, 6}, 2, 1),
+                arguments(new int[]{1, 3, 5, 6}, 7, 4),
+                arguments(new int[]{1, 3, 5, 6}, 0, 0)
+        );
+    }
+}
