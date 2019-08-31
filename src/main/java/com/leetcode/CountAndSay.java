8d67f4f4d29cc18ae7f84f731f06f3ccc359bb7e
Carl <zxfspace@163.com>
2019-08-31 11:07:46 +0800

    count and say(1ms)

--- src/main/java/com/leetcode/CountAndSay.java
+++ src/main/java/com/leetcode/CountAndSay.java
@@ -4,34 +4,22 @@ package com.leetcode;
  * @author carl
  */
 public class CountAndSay {
-    static String[] seq = new String[31];
-
-    static {
-        seq[0] = "";
-        seq[1] = "1";
-        seq[2] = "11";
-        seq[3] = "21";
-        for (int i = 4; i < seq.length; i++) {
-            StringBuilder sb = new StringBuilder();
-            int j = 0;
-            char pre = seq[i - 1].charAt(0);
-            while (j < seq[i - 1].length()) {
-                int count = 0;
-                while (j < seq[i - 1].length() && seq[i - 1].charAt(j) == pre) {
-                    count++;
-                    j++;
-                }
-                sb.append(count);
-                sb.append(pre);
-                if (j < seq[i - 1].length()) {
-                    pre = seq[i - 1].charAt(j);
-                }
-            }
-            seq[i] = sb.toString();
-        }
-    }
 
     public String countAndSay(int n) {
-        return seq[n];
+        if (n == 1) return "1";
+        String pre = countAndSay(n - 1);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < pre.length(); ) {
+            int l = i;
+            int r = i + 1;
+            while (r < pre.length() && pre.charAt(r) == pre.charAt(l)) {
+                r++;
+            }
+            sb.append(r - l);
+            sb.append(pre.charAt(l));
+            i = r;
+        }
+        return sb.toString();
     }
 }
