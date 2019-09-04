93c2596ed12bf3662b7d291efd5b2ee7b4edb2ca
carl <xinfeng.zhao2@wework.com>
2019-09-04 18:25:22 +0800

    pow

---
+++ src/main/java/com/leetcode/Pow.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+/**
+ * @auther carl
+ */
+public class Pow {
+
+    public double myPow(double x, int n) {
+
+        return 0.0;
+    }
+}
---
+++ src/test/java/com/leetcode/PowTest.java
@@ -0,0 +1,31 @@
+package com.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+/**
+ * @auther carl
+ */
+public class PowTest {
+    Pow pow = new Pow();
+
+    @ParameterizedTest
+    @MethodSource("source")
+    public void test(double x, int n, double expect) {
+        assertEquals(expect, pow.myPow(x, n));
+    }
+
+    static Stream<Arguments> source() {
+        return Stream.of(
+                arguments(2.00000, 10, 1024.00000),
+                arguments(2.10000, 3, 9.26100),
+                arguments(2.00000, -2, 0.25000)
+        );
+    }
+}
