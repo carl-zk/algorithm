7fe8e26342d2d2eefcd796c06352cf14c30f5805
Carl <zxfspace@163.com>
2019-09-05 08:47:27 +0800

    pow

--- src/test/java/com/leetcode/PowTest.java
+++ src/test/java/com/leetcode/PowTest.java
@@ -1,9 +1,11 @@
 package com.leetcode;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.math.BigInteger;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -23,9 +25,19 @@ public class PowTest {
 
     static Stream<Arguments> source() {
         return Stream.of(
+                arguments(2.00000, -2147483648, 0),
                 arguments(2.00000, 10, 1024.00000),
                 arguments(2.10000, 3, 9.26100),
-                arguments(2.00000, -2, 0.25000)
+                arguments(2.00000, -2, 0.25000),
+                arguments(1.00000, -2147483648, 1.00),
+                arguments(-1.00000, -2147483648, 1.00)
         );
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(-1 * Integer.MIN_VALUE);
+        System.out.println(1 + Integer.MIN_VALUE);
+        System.out.println(BigInteger.valueOf(-2147483648L).multiply(BigInteger.valueOf(-1)));
     }
 }
