1d318c0fe5035c295ea612b6ffb84663cd17ed38
Carl <zxfspace@163.com>
2019-09-06 08:55:44 +0800

    Spiral Matrix

--- src/main/java/com/leetcode/onehundred/SpiralMatrix.java
+++ src/main/java/com/leetcode/onehundred/SpiralMatrix.java
@@ -36,34 +36,27 @@ public class SpiralMatrix {
         ans = new LinkedList<>();
         if (matrix.length == 0) return ans;
 
-        int cnt = matrix.length * matrix[0].length;
         int upX = 0, bottomX = matrix.length - 1, leftY = 0, rightY = matrix[0].length - 1;
 
-        while (cnt > 0) {
+        while (upX <= bottomX && leftY <= rightY) {
             for (int i = leftY; i <= rightY; i++) {
                 ans.add(matrix[upX][i]);
-                cnt--;
             }
-            if (cnt == 0) return ans;
-            upX++;
-            for (int i = upX; i <= bottomX; i++) {
+            for (int i = upX + 1; i <= bottomX; i++) {
                 ans.add(matrix[i][rightY]);
-                cnt--;
             }
-            if (cnt == 0) return ans;
-            rightY--;
-            for (int i = rightY; i >= leftY; i--) {
-                ans.add(matrix[bottomX][i]);
-                cnt--;
+            if (upX < bottomX && leftY < rightY) {
+                for (int i = rightY - 1; i > leftY; i--) {
+                    ans.add(matrix[bottomX][i]);
+                }
+                for (int i = bottomX; i > upX; i--) {
+                    ans.add(matrix[i][leftY]);
+                }
             }
-            if (cnt == 0) return ans;
+            upX++;
             bottomX--;
-            for (int i = bottomX; i >= upX; i--) {
-                ans.add(matrix[i][leftY]);
-                cnt--;
-            }
-            if (cnt == 0) return ans;
             leftY++;
+            rightY--;
         }
         return ans;
     }
