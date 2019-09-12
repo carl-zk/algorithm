ed38778838847d03c7adb45a636c39e3080e0855
Carl <zxfspace@163.com>
2019-09-12 23:18:08 +0800

    maximal rectangle (I DID IT!)

--- src/main/java/com/leetcode/onehundred/MaximalRectangle.java
+++ src/main/java/com/leetcode/onehundred/MaximalRectangle.java
@@ -1,6 +1,21 @@
 package com.leetcode.onehundred;
 
 /**
+ * https://leetcode.com/problems/maximal-rectangle/
+ *
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+ *
+ * Example:
+ *
+ * Input:
+ * [
+ *   ["1","0","1","0","0"],
+ *   ["1","0","1","1","1"],
+ *   ["1","1","1","1","1"],
+ *   ["1","0","0","1","0"]
+ * ]
+ * Output: 6
+ *
  * @auther carl
  */
 public class MaximalRectangle {
@@ -8,55 +23,97 @@ public class MaximalRectangle {
     public int maximalRectangle(char[][] matrix) {
         if (matrix.length == 0 || matrix[0].length == 0) return 0;
 
-        int[][] dpR = new int[matrix.length][matrix[0].length];
-        int[][] dpC = new int[matrix.length][matrix[0].length];
-        int[][] dpAngle = new int[matrix.length][matrix[0].length];
         int ans = 0;
 
-        for (int i = 0; i < matrix[0].length; i++) {
-            if (matrix[0][i] == '1') {
-                if (i == 0) {
-                    dpR[0][i] = 1;
-                } else {
-                    dpR[0][i] = 1 + dpR[0][i - 1];
-                }
-                if (dpR[0][i] > ans) {
-                    ans = dpR[0][i];
-                }
-            }
-        }
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        int[] heights = new int[matrix.length];
 
         for (int i = 0; i < matrix.length; i++) {
-            if (matrix[i][0] == '1') {
-                if (i == 0) {
-                    dpC[i][0] = 1;
-                } else {
-                    dpC[i][0] = 1 + dpC[i - 1][0];
-                }
-                if (dpC[i][0] > ans) {
-                    ans = dpC[i][0];
-                }
-            }
-        }
-
-        if (matrix[0][0] == '1') {
-            dpAngle[0][0] = 1;
-        }
-
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 1; j < matrix[0].length; j++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == '1') {
-                    dpR[i][j] = dpR[i][j - 1] + 1;
-                    dpC[i][j] = dpC[i - 1][j] + 1;
-                    if (matrix[i][j - 1] == '1' && matrix[i - 1][j] == '1') {
-                        dpAngle[i][j] = dpAngle[i - 1][j - 1] + 1;
-                    } else {
-                        dpAngle[i][j] = 1;
+                    dp[i][j] = j == 0 ? 1 : dp[i][j - 1] + 1;
+                    int len = 0;
+                    for (int k = i; k > -1 && dp[k][j] > 0; k--) {
+                        heights[len++] = dp[k][j];
                     }
-
+                    int rec = solve(heights, 0, len - 1);
+                    if (rec > ans) {
+                        ans = rec;
+                    }
                 }
             }
         }
-        return 0;
+
+        return ans;
+    }
+
+    private int solve(int[] heights, int start, int end) {
+        if (start > end) {
+            return 0;
+        }
+        boolean asc = true;
+        boolean desc = true;
+        int indexMin = start;
+        for (int i = start; i <= end; i++) {
+            if (i != start && heights[i] > heights[i - 1]) {
+                desc = false;
+            }
+            if (i != start && heights[i] < heights[i - 1]) {
+                asc = false;
+            }
+            if (heights[i] < heights[indexMin]) {
+                indexMin = i;
+            }
+        }
+        if (asc) {
+            int ans = 0;
+            for (int i = start, area; i <= end; i++) {
+                area = (end - i + 1) * heights[i];
+                if (area > ans) {
+                    ans = area;
+                }
+            }
+            return ans;
+        }
+        if (desc) {
+            int ans = 0;
+            for (int i = start, area; i <= end; i++) {
+                area = (i - start + 1) * heights[i];
+                if (area > ans) {
+                    ans = area;
+                }
+            }
+            return ans;
+        }
+        int leftMax = solve(heights, start, indexMin - 1);
+        int rightMax = solve(heights, indexMin + 1, end);
+        int l = indexMin;
+        while (l - 1 >= start && heights[l - 1] >= heights[indexMin]) l--;
+        int r = indexMin;
+        while (r + 1 <= end && heights[r + 1] >= heights[indexMin]) r++;
+        int area = (r - l + 1) * heights[indexMin];
+        return Math.max(area, Math.max(leftMax, rightMax));
+    }
+
+    public static void main(String[] args) {
+        MaximalRectangle mr = new MaximalRectangle();
+        char[][] m = new char[][]{
+                {'0', '1', '0', '1', '0', '1', '0', '1', '1', '0', '1', '1', '0', '1', '0'},
+                {'0', '1', '1', '1', '1', '1', '0', '1', '1', '0', '1', '1', '1', '1', '0'},
+                {'0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '0', '1', '1'},
+                {'1', '0', '1', '1', '0', '1', '0', '0', '1', '1', '1', '0', '0', '0', '1'},
+                {'0', '1', '0', '1', '1', '1', '0', '1', '0', '1', '1', '0', '0', '1', '1'},
+                {'1', '1', '1', '1', '0', '1', '1', '0', '1', '1', '1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '0', '1', '0', '1', '0', '1', '1', '1', '1', '1', '0'},
+                {'0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '1', '1', '1', '0'},
+                {'0', '1', '1', '1', '0', '1', '0', '0', '0', '0', '1', '1', '1', '1', '1'},
+                {'1', '1', '1', '1', '1', '0', '1', '0', '1', '1', '1', '0', '0', '1', '1'},
+                {'0', '1', '1', '1', '0', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
+                {'1', '1', '1', '0', '0', '0', '1', '1', '1', '0', '1', '0', '0', '1', '1'},
+                {'1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '0', '1', '1', '1', '1'},
+                {'1', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1'},
+                {'1', '1', '1', '0', '1', '0', '1', '0', '1', '0', '1', '1', '1', '1', '1'}
+        };
+        System.out.println(mr.maximalRectangle(m));
     }
 }
