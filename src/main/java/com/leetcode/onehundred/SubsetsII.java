ea26c507bd11e92c4faaea11c3913040d2d300a2
Carl <zxfspace@163.com>
2019-09-14 10:46:43 +0800

    subsets II

---
+++ src/main/java/com/leetcode/onehundred/SubsetsII.java
@@ -0,0 +1,54 @@
+package com.leetcode.onehundred;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/subsets-ii/
+ * <p>
+ * Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+ * <p>
+ * Note: The solution set must not contain duplicate subsets.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [1,2,2]
+ * Output:
+ * [
+ * [2],
+ * [1],
+ * [1,2,2],
+ * [2,2],
+ * [1,2],
+ * []
+ * ]
+ *
+ * @author carl
+ */
+public class SubsetsII {
+    List<List<Integer>> ans;
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        ans = new LinkedList<>();
+
+        Arrays.sort(nums);
+
+        solve(nums, 0, new ArrayList<>());
+        return ans;
+    }
+
+    private void solve(int[] nums, int start, ArrayList<Integer> temp) {
+        ans.add(new ArrayList(temp));
+
+        int sz = temp.size();
+        for (int i = start; i < nums.length; i++) {
+            if (i == start || nums[i] > nums[i - 1]) {
+                temp.add(sz, nums[i]);
+                solve(nums, i + 1, temp);
+                temp.remove(sz);
+            }
+        }
+    }
+}
