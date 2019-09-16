c1d4b448644b5debee20106f18fa2efda169e4ef
carl <xinfeng.zhao2@wework.com>
2019-09-16 14:07:17 +0800

    interleaving string

--- src/main/java/com/leetcode/onehundred/InterleavingString.java
+++ src/main/java/com/leetcode/onehundred/InterleavingString.java
@@ -17,27 +17,27 @@ package com.leetcode.onehundred;
  * @auther carl
  */
 public class InterleavingString {
-    boolean[][] dp;
+    boolean[] dp;
 
     public boolean isInterleave(String s1, String s2, String s3) {
         if (s3.length() != s1.length() + s2.length()) return false;
 
-        dp = new boolean[s1.length() + 1][s2.length() + 1];
+        dp = new boolean[s2.length() + 1];
         for (int i = 0; i <= s1.length(); i++) {
             for (int j = 0; j <= s2.length(); j++) {
                 if (i == 0 && j == 0) {
-                    dp[i][j] = true;
+                    dp[j] = true;
                 } else if (i == 0) {
-                    dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                    dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
                 } else if (j == 0) {
-                    dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
                 } else {
-                    dp[i][j] = s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j]
-                            || s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1];
+                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)
+                            || dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
                 }
             }
         }
-        return dp[s1.length()][s2.length()];
+        return dp[s2.length()];
     }
 
     public static void main(String[] args) {
