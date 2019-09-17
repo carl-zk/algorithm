97b2b870006df938fa293a1b27ae4dda2cbc7e3a
carl <xinfeng.zhao2@wework.com>
2019-09-17 18:10:30 +0800

    convert sorted list to binary search tree

---
+++ src/main/java/com/leetcode/onehundredfifty/ConvertSortedListToBinarySearchTree.java
@@ -0,0 +1,13 @@
+package com.leetcode.onehundredfifty;
+
+/**
+ * @auther carl
+ */
+public class ConvertSortedListToBinarySearchTree {
+
+    public TreeNode sortedListToBST(ListNode head) {
+
+    }
+
+
+}
---
+++ src/main/java/com/leetcode/onehundredfifty/ListNode.java
@@ -0,0 +1,13 @@
+package com.leetcode.onehundredfifty;
+
+/**
+ * @auther carl
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
