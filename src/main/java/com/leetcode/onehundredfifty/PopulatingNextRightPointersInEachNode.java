7b4a3d3bc26f64645a0f1b44c88f6197fbe2da79
Carl <zxfspace@163.com>
2019-09-19 00:19:25 +0800

    populating next right pointers in each node

--- src/main/java/com/leetcode/onehundredfifty/PopulatingNextRightPointersInEachNode.java
+++ src/main/java/com/leetcode/onehundredfifty/PopulatingNextRightPointersInEachNode.java
@@ -1,62 +1,56 @@
 package com.leetcode.onehundredfifty;
 
-import java.util.ArrayList;
-
 /**
  * https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
- *
+ * <p>
  * You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
- *
+ * <p>
  * struct Node {
- *   int val;
- *   Node *left;
- *   Node *right;
- *   Node *next;
+ * int val;
+ * Node *left;
+ * Node *right;
+ * Node *next;
  * }
  * Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
- *
+ * <p>
  * Initially, all next pointers are set to NULL.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example:
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Input: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
- *
+ * <p>
  * Output: {"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
- *
+ * <p>
  * Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B.
- *
- *
+ * <p>
+ * <p>
  * Note:
- *
+ * <p>
  * You may only use constant extra space.
  * Recursive approach is fine, implicit stack space does not count as extra space for this problem.
  *
  * @auther carl
  */
 public class PopulatingNextRightPointersInEachNode {
-    ArrayList<Node> arr;
+
 
     public Node connect(Node root) {
-        arr = new ArrayList<>();
-
-        solve(root, 1);
-        return root;
-    }
-
-    private void solve(Node root, int level) {
-        if (root == null) return;
-        if (arr.size() < level) {
-            arr.add(root);
-        } else {
-            arr.get(level - 1).next = root;
-            arr.set(level - 1, root);
+        if (root == null) return root;
+        if (root.left != null) {
+            root.left.next = root.right;
         }
-        solve(root.left, level + 1);
-        solve(root.right, level + 1);
+        if (root.right != null) {
+            if (root.next != null) {
+                root.right.next = root.next.left;
+            }
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
     }
 
     public static void main(String[] args) {
