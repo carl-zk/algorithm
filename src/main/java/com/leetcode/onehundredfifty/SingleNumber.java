d5ccad7e2a402e1d92a49e3b632fbbf140959618
carl <xinfeng.zhao2@wework.com>
2019-09-20 10:27:41 +0800

    single number (crap)

---
+++ src/main/java/com/leetcode/onehundredfifty/SingleNumber.java
@@ -0,0 +1,32 @@
+package com.leetcode.onehundredfifty;
+
+/**
+ * https://leetcode.com/problems/single-number/
+ * <p>
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ * <p>
+ * Note:
+ * <p>
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [2,2,1]
+ * Output: 1
+ * Example 2:
+ * <p>
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ *
+ * @auther carl
+ */
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+}
