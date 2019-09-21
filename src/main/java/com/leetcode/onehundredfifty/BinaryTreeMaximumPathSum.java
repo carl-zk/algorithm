4d44bcdc4b86bc35a545deb080eb70cf01cb7370
Carl <zxfspace@163.com>
2019-09-21 11:08:39 +0800

    binary tree maximum path sum

--- src/main/java/com/leetcode/onehundredfifty/BinaryTreeMaximumPathSum.java
+++ src/main/java/com/leetcode/onehundredfifty/BinaryTreeMaximumPathSum.java
@@ -2,30 +2,30 @@ package com.leetcode.onehundredfifty;
 
 /**
  * https://leetcode.com/problems/binary-tree-maximum-path-sum/
- *
+ * <p>
  * Given a non-empty binary tree, find the maximum path sum.
- *
+ * <p>
  * For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: [1,2,3]
- *
- *        1
- *       / \
- *      2   3
- *
+ * <p>
+ * 1
+ * / \
+ * 2   3
+ * <p>
  * Output: 6
  * Example 2:
- *
+ * <p>
  * Input: [-10,9,20,null,null,15,7]
- *
- *    -10
- *    / \
- *   9  20
- *     /  \
- *    15   7
- *
+ * <p>
+ * -10
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * <p>
  * Output: 42
  *
  * @auther carl
@@ -41,48 +41,28 @@ public class BinaryTreeMaximumPathSum {
     }
 
     private long solve(TreeNode root) {
+        if (root == null) return Integer.MIN_VALUE;
+
         if (root.val > ans) {
             ans = root.val;
         }
         if (root.left == null && root.right == null) {
             return root.val;
         }
-        if (root.left != null && root.right != null) {
-            long left = solve(root.left);
-            if (left > ans) {
-                ans = left;
-            }
-            long right = solve(root.right);
-            if (right > ans) {
-                ans = right;
-            }
-            long max = Math.max(root.val, Math.max(root.val + left, root.val + right));
-            if (max > ans) {
-                ans = max;
-            }
-            if (root.val + left + right > ans) {
-                ans = root.val + left + right;
-            }
-            return max;
-        }
-        if (root.left != null) {
-            long left = solve(root.left);
-            if (left > ans) {
-                ans = left;
-            }
-            long max = Math.max(root.val, root.val + left);
-            if (max > ans) {
-                ans = max;
-            }
-            return max;
-        }
+        long left = solve(root.left);
         long right = solve(root.right);
+        if (left > ans) {
+            ans = left;
+        }
         if (right > ans) {
             ans = right;
         }
-        long max = Math.max(root.val, root.val + right);
+        long max = Math.max(root.val, Math.max(root.val + left, root.val + right));
         if (max > ans) {
             ans = max;
+        }
+        if (root.val + left + right > ans) {
+            ans = root.val + left + right;
         }
         return max;
     }
