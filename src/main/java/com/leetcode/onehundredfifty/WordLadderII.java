9c24b638c7395a3c7e309401832385905bac194b
carl <xinfeng.zhao2@wework.com>
2019-09-23 14:11:15 +0800

    word ladder 2 (390ms, finally AC)

--- src/main/java/com/leetcode/onehundredfifty/WordLadderII.java
+++ src/main/java/com/leetcode/onehundredfifty/WordLadderII.java
@@ -1,44 +1,91 @@
 package com.leetcode.onehundredfifty;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
+ * https://leetcode.com/problems/word-ladder-ii/
+ * <p>
+ * Given two words (beginWord and endWord), and a dictionary's word list, find all shortest transformation sequence(s) from beginWord to endWord, such that:
+ * <p>
+ * Only one letter can be changed at a time
+ * Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+ * Note:
+ * <p>
+ * Return an empty list if there is no such transformation sequence.
+ * All words have the same length.
+ * All words contain only lowercase alphabetic characters.
+ * You may assume no duplicates in the word list.
+ * You may assume beginWord and endWord are non-empty and are not the same.
+ * Example 1:
+ * <p>
+ * Input:
+ * beginWord = "hit",
+ * endWord = "cog",
+ * wordList = ["hot","dot","dog","lot","log","cog"]
+ * <p>
+ * Output:
+ * [
+ * ["hit","hot","dot","dog","cog"],
+ * ["hit","hot","lot","log","cog"]
+ * ]
+ * Example 2:
+ * <p>
+ * Input:
+ * beginWord = "hit"
+ * endWord = "cog"
+ * wordList = ["hot","dot","dog","lot","log"]
+ * <p>
+ * Output: []
+ * <p>
+ * Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
+ *
  * @author carl
  */
 public class WordLadderII {
     List<List<String>> ans;
-    int[] distb;
     ArrayList<Integer>[] distwl;
     Set<Integer> dest;
-    boolean[] visited;
-    String bw;
-    List<String> wl;
+    Queue<ArrayList<Integer>> que;
+    int[] soFar;
     int minLen;
-    int[] sofar;
-
-    ArrayList<ArrayList<String>>[] suffix;
-    int[] minSz;
 
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        distb = new int[wordList.size()];
+
+        init(wordList);
+        calEachDistance(endWord, wordList);
+
+        if (dest.isEmpty()) return Collections.emptyList();
+
+        for (int i = 0; i < wordList.size(); i++) {
+            String w = wordList.get(i);
+            int d = 0;
+            for (int j = 0; j < beginWord.length(); j++) {
+                if (beginWord.charAt(j) != w.charAt(j)) {
+                    d++;
+                }
+            }
+            if (d == 1) {
+                ArrayList<Integer> a = new ArrayList<>();
+                a.add(i);
+                que.add(a);
+            }
+        }
+
+        bfs(beginWord, wordList);
+        return ans;
+    }
+
+    private void init(List<String> wordList) {
+        ans = new LinkedList<>();
         distwl = new ArrayList[wordList.size()];
         dest = new HashSet<>();
-        visited = new boolean[wordList.size()];
-        bw = beginWord;
-        wl = wordList;
-        minLen = Integer.MAX_VALUE;
-        suffix = new ArrayList[wordList.size()];
-        minSz = new int[wordList.size()];
-        Arrays.fill(minSz, wordList.size() << 1);
-        sofar = new int[wordList.size()];
-        Arrays.fill(sofar, Integer.MAX_VALUE);
+        minLen = wordList.size() << 1;
+        que = new LinkedList<>();
+        soFar = new int[wordList.size()];
+        Arrays.fill(soFar, wordList.size() << 1);
+    }
 
+    private void calEachDistance(String endWord, List<String> wordList) {
         for (int i = 0; i < wordList.size(); i++) {
             String w1 = wordList.get(i);
             for (int j = i + 1; j < wordList.size(); j++) {
@@ -64,54 +111,34 @@ public class WordLadderII {
                 dest.add(i);
             }
         }
-
-        if (dest.isEmpty()) return Collections.emptyList();
-
-        ans = new LinkedList<>();
-        ArrayList<String> temp = new ArrayList<>();
-
-        for (int i = 0; i < wordList.size(); i++) {
-            String w = wordList.get(i);
-            for (int j = 0; j < beginWord.length(); j++) {
-                if (beginWord.charAt(j) != w.charAt(j)) {
-                    distb[i]++;
-                }
-            }
-            if (distb[i] == 1) {
-                visited[i] = true;
-                temp.add(wl.get(i));
-                sofar[i] = 1;
-                solve(i, temp);
-                temp.remove(0);
-                visited[i] = false;
-            }
-        }
-        return ans;
     }
 
-    private void solve(int start, ArrayList<String> temp) {
-        if (start < 0 || temp.size() > minLen || sofar[start] < temp.size()) return;
-        if (dest.contains(start)) {
-            if (minLen < temp.size()) return;
-            if (minLen > temp.size()) {
-                ans.clear();
+    private void bfs(String beginWord, List<String> wordList) {
+        while (!que.isEmpty()) {
+            ArrayList<Integer> a = que.poll();
+            if (a.size() > minLen) return;
+            if (dest.contains(a.get(a.size() - 1))) {
+                minLen = a.size();
+                List<String> list = new LinkedList<>();
+                list.add(beginWord);
+                for (int i = 0; i < a.size(); i++) {
+                    list.add(wordList.get(a.get(i)));
+                }
+                ans.add(list);
+                continue;
             }
-            minLen = temp.size();
-            ArrayList<String> one = new ArrayList<>(temp);
-            one.add(0, bw);
-            ans.add(one);
-            return;
-        }
-        sofar[start] = temp.size();
-        if (distwl[start] == null) return;
-        for (int i = 0; i < distwl[start].size(); i++) {
-            int j = distwl[start].get(i);
-            if (!visited[j] && sofar[j] >= temp.size() + 1) {
-                visited[j] = true;
-                temp.add(wl.get(j));
-                solve(j, temp);
-                temp.remove(temp.size() - 1);
-                visited[j] = false;
+            Set<Integer> used = new HashSet<>();
+            for (int i = 0; i < a.size(); i++) {
+                used.add(a.get(i));
+            }
+            int j = a.get(a.size() - 1);
+            for (int i = 0; i < distwl[j].size(); i++) {
+                if (!used.contains(distwl[j].get(i)) && soFar[distwl[j].get(i)] >= a.size()) {
+                    soFar[distwl[j].get(i)] = a.size();
+                    ArrayList<Integer> next = new ArrayList<>(a);
+                    next.add(distwl[j].get(i));
+                    que.add(next);
+                }
             }
         }
     }
