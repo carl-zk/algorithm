a4f70ef5dc77631c8bee6e2ba65b4fa4d531938d
carl <xinfeng.zhao2@wework.com>
2019-09-25 14:09:36 +0800

    clone graph (no debug, once AC)

---
+++ src/main/java/com/leetcode/onehundredfifty/CloneGraph.java
@@ -0,0 +1,87 @@
+package com.leetcode.onehundredfifty;
+
+import java.util.*;
+
+/**
+ * https://leetcode.com/problems/clone-graph/
+ * <p>
+ * Given a reference of a node in a connected undirected graph, return a deep copy (clone) of the graph. Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
+ * <p>
+ * <p>
+ * <p>
+ * Example:
+ * <p>
+ * <p>
+ * <p>
+ * Input:
+ * {"$id":"1","neighbors":[{"$id":"2","neighbors":[{"$ref":"1"},{"$id":"3","neighbors":[{"$ref":"2"},{"$id":"4","neighbors":[{"$ref":"3"},{"$ref":"1"}],"val":4}],"val":3}],"val":2},{"$ref":"4"}],"val":1}
+ * <p>
+ * Explanation:
+ * Node 1's value is 1, and it has two neighbors: Node 2 and 4.
+ * Node 2's value is 2, and it has two neighbors: Node 1 and 3.
+ * Node 3's value is 3, and it has two neighbors: Node 2 and 4.
+ * Node 4's value is 4, and it has two neighbors: Node 1 and 3.
+ * <p>
+ * <p>
+ * Note:
+ * <p>
+ * The number of nodes will be between 1 and 100.
+ * The undirected graph is a simple graph, which means no repeated edges and no self-loops in the graph.
+ * Since the graph is undirected, if node p has node q as neighbor, then node q must have node p as neighbor too.
+ * You must return the copy of the given node as a reference to the cloned graph.
+ *
+ * @auther carl
+ */
+public class CloneGraph {
+    Set<Node> visited = new HashSet<>();
+    Map<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        buildMap(node);
+        doClone(node);
+        return map.get(node);
+    }
+
+    private void buildMap(Node node) {
+        if (node == null || map.get(node) != null) {
+            return;
+        }
+        Node copy = new Node();
+        copy.val = node.val;
+        map.put(node, copy);
+
+        if (node.neighbors != null) {
+            for (Node o : node.neighbors) {
+                buildMap(o);
+            }
+        }
+    }
+
+    private void doClone(Node node) {
+        if (node == null || visited.contains(node)) return;
+        visited.add(node);
+        Node copy = map.get(node);
+        if (node.neighbors != null) {
+            List<Node> nodes = new ArrayList<>();
+            for (Node o : node.neighbors) {
+                doClone(o);
+                nodes.add(map.get(o));
+            }
+            copy.neighbors = nodes;
+        }
+    }
+
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+        }
+
+        public Node(int _val, List<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+}
