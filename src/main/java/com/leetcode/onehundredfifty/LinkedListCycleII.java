669c891b756215e3cc906f1aae5101626ed429ad
Carl <zxfspace@163.com>
2019-09-26 21:33:43 +0800

    linked list cycle 2

--- src/main/java/com/leetcode/onehundredfifty/LinkedListCycleII.java
+++ src/main/java/com/leetcode/onehundredfifty/LinkedListCycleII.java
@@ -1,42 +1,39 @@
 package com.leetcode.onehundredfifty;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * https://leetcode.com/problems/linked-list-cycle-ii/
- *
+ * <p>
  * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
- *
+ * <p>
  * To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
- *
+ * <p>
  * Note: Do not modify the linked list.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: head = [3,2,0,-4], pos = 1
  * Output: tail connects to node index 1
  * Explanation: There is a cycle in the linked list, where tail connects to the second node.
- *
- *
+ * <p>
+ * <p>
  * Example 2:
- *
+ * <p>
  * Input: head = [1,2], pos = 0
  * Output: tail connects to node index 0
  * Explanation: There is a cycle in the linked list, where tail connects to the first node.
- *
- *
+ * <p>
+ * <p>
  * Example 3:
- *
+ * <p>
  * Input: head = [1], pos = -1
  * Output: no cycle
  * Explanation: There is no cycle in the linked list.
- *
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
+ * <p>
  * Follow-up:
  * Can you solve it without using extra space?
  *
@@ -45,12 +42,22 @@ import java.util.Set;
 public class LinkedListCycleII {
 
     public ListNode detectCycle(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
-        while (head != null) {
-            if (set.contains(head)) return head;
-            set.add(head);
-            head = head.next;
+        if (head == null || head.next == null) return null;
+        ListNode slow = head, fast = head.next.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) return null;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return null;
+        fast = head.next;
+        for (ListNode p = slow.next; p != slow; p = p.next) {
+            fast = fast.next;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
     }
 }
