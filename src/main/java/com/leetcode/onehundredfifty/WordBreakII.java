21d05d33febac84b6a2d402504c808ea1e28e24c
carl <xinfeng.zhao2@wework.com>
2019-09-26 17:07:12 +0800

    word break 2

--- src/main/java/com/leetcode/onehundredfifty/WordBreakII.java
+++ src/main/java/com/leetcode/onehundredfifty/WordBreakII.java
@@ -3,24 +3,74 @@ package com.leetcode.onehundredfifty;
 import java.util.*;
 
 /**
+ * https://leetcode.com/problems/word-break-ii/
+ * <p>
+ * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences.
+ * <p>
+ * Note:
+ * <p>
+ * The same word in the dictionary may be reused multiple times in the segmentation.
+ * You may assume the dictionary does not contain duplicate words.
+ * Example 1:
+ * <p>
+ * Input:
+ * s = "catsanddog"
+ * wordDict = ["cat", "cats", "and", "sand", "dog"]
+ * Output:
+ * [
+ * "cats and dog",
+ * "cat sand dog"
+ * ]
+ * Example 2:
+ * <p>
+ * Input:
+ * s = "pineapplepenapple"
+ * wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+ * Output:
+ * [
+ * "pine apple pen apple",
+ * "pineapple pen apple",
+ * "pine applepen apple"
+ * ]
+ * Explanation: Note that you are allowed to reuse a dictionary word.
+ * Example 3:
+ * <p>
+ * Input:
+ * s = "catsandog"
+ * wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * Output:
+ * []
+ *
  * @auther carl
  */
 public class WordBreakII {
-    Set<String> dict;
-    Map<Integer, List<String>> map;
+    Map<String, List<String>> map;
 
     public List<String> wordBreak(String s, List<String> wordDict) {
-        dict = new HashSet<>(wordDict);
         map = new HashMap<>();
-
-        return map.get(s);
+        return solve(s, wordDict);
     }
 
-    private List<String> solve(String s) {
+    private List<String> solve(String s, List<String> wordDict) {
         if (map.containsKey(s)) {
             return map.get(s);
         }
+        List<String> ans = new ArrayList<>();
+        if (s.length() == 0) {
+            ans.add("");
+            return ans;
+        }
 
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                List<String> suffix = solve(s.substring(word.length()), wordDict);
+                for (String suf : suffix) {
+                    ans.add(word + (suf.length() == 0 ? "" : " ") + suf);
+                }
+            }
+        }
+        map.put(s, ans);
+        return ans;
     }
 
     public static void main(String[] args) {
