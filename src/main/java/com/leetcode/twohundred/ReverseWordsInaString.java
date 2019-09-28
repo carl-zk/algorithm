ed29cc9a91c7f78da8c31d1da29160e883ef9090
Carl <zxfspace@163.com>
2019-09-28 23:23:02 +0800

    reverse words in a string

---
+++ src/main/java/com/leetcode/twohundred/ReverseWordsInaString.java
@@ -0,0 +1,53 @@
+package com.leetcode.twohundred;
+
+/**
+ * https://leetcode.com/problems/reverse-words-in-a-string/
+ * <p>
+ * Given an input string, reverse the string word by word.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "the sky is blue"
+ * Output: "blue is sky the"
+ * Example 2:
+ * <p>
+ * Input: "  hello world!  "
+ * Output: "world! hello"
+ * Explanation: Your reversed string should not contain leading or trailing spaces.
+ * Example 3:
+ * <p>
+ * Input: "a good   example"
+ * Output: "example good a"
+ * Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ * <p>
+ * <p>
+ * Note:
+ * <p>
+ * A word is defined as a sequence of non-space characters.
+ * Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+ * You need to reduce multiple spaces between two words to a single space in the reversed string.
+ * <p>
+ * <p>
+ * Follow up:
+ * <p>
+ * For C programmers, try to solve it in-place in O(1) extra space.
+ *
+ * @author carl
+ */
+public class ReverseWordsInaString {
+
+    public String reverseWords(String s) {
+        return solve(s.toCharArray(), 0);
+    }
+
+    private String solve(char[] chars, int start) {
+        while (start < chars.length && chars[start] == ' ') start++;
+        if (start == chars.length) return "";
+        int end = start;
+        while (end < chars.length && chars[end] != ' ') end++;
+        String pre = solve(chars, end);
+        return pre + (pre.length() == 0 ? "" : " ") + String.valueOf(chars, start, end - start);
+    }
+}
