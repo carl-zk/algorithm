875de9e18947c1e89335bd664fb5a1e131ef73e8
carl <xinfeng.zhao2@wework.com>
2019-09-29 11:39:04 +0800

    intersection of two linkedlists

---
+++ src/main/java/com/leetcode/twohundred/ListNode.java
@@ -0,0 +1,14 @@
+package com.leetcode.twohundred;
+
+/**
+ * @auther carl
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
