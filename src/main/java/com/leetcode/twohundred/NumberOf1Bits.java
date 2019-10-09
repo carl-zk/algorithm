78e3a211ecd333a690512fce635c6f83fdac9386
carl <xinfeng.zhao2@wework.com>
2019-10-09 16:58:54 +0800

    number of 1 bits

--- src/main/java/com/leetcode/twohundred/NumberOf1Bits.java
+++ src/main/java/com/leetcode/twohundred/NumberOf1Bits.java
@@ -29,9 +29,9 @@ public class NumberOf1Bits {
 
     public int hammingWeight(int n) {
         int ans = 0;
-        for (int i = 0; i < 32; i++) {
-            ans += (n & 1);
-            n >>= 1;
+        while (n != 0) {
+            ans++;
+            n &= (n - 1);
         }
         return ans;
     }
