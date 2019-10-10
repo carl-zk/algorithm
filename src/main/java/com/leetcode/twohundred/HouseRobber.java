1d9b31886c808fbfa4954236ab55d4ca554af0dd
carl <xinfeng.zhao2@wework.com>
2019-10-10 11:44:07 +0800

    house robber

--- src/main/java/com/leetcode/twohundred/HouseRobber.java
+++ src/main/java/com/leetcode/twohundred/HouseRobber.java
@@ -23,26 +23,16 @@ package com.leetcode.twohundred;
  * @auther carl
  */
 public class HouseRobber {
-    int ans;
-    int[] mem;
 
     public int rob(int[] nums) {
-        ans = 0;
-        mem = new int[nums.length];
-        int cur = 0;
-        while (cur < nums.length && nums[cur] == 0) cur++;
-        solve(nums, cur, 0);
-        return ans;
-    }
-
-    private void solve(int[] nums, int cur, int sum) {
-        if (cur >= nums.length) {
-            ans = Math.max(ans, sum);
-            return;
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
         }
-        if (sum < mem[cur]) return;
-        mem[cur] = sum;
-        solve(nums, cur + 2, sum + nums[cur]);
-        solve(nums, cur + 1, sum);
+        return dp[nums.length - 1];
     }
 }
