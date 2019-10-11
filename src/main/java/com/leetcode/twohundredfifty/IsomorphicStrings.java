98466ea35f685b424e5d7763151916deaa65169c
carl <xinfeng.zhao2@wework.com>
2019-10-11 11:35:08 +0800

    Isomorphic strings

---
+++ src/main/java/com/leetcode/twohundredfifty/IsomorphicStrings.java
@@ -0,0 +1,47 @@
+package com.leetcode.twohundredfifty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/isomorphic-strings/
+ * <p>
+ * Given two strings s and t, determine if they are isomorphic.
+ * <p>
+ * Two strings are isomorphic if the characters in s can be replaced to get t.
+ * <p>
+ * All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "egg", t = "add"
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: s = "foo", t = "bar"
+ * Output: false
+ * Example 3:
+ * <p>
+ * Input: s = "paper", t = "title"
+ * Output: true
+ * Note:
+ * You may assume both s and t have the same length.
+ *
+ * @auther carl
+ */
+public class IsomorphicStrings {
+
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i)) && t.charAt(i) != map.get(s.charAt(i))) {
+                return false;
+            } else if (!map.containsKey(s.charAt(i)) && map.containsValue(t.charAt(i))) {
+                return false;
+            } else {
+                map.put(s.charAt(i), t.charAt(i));
+            }
+        }
+        return true;
+    }
+}
