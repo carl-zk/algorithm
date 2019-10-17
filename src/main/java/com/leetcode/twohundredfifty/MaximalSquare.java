73d8f78a6afab511d8e9c0063f312ac4533e129a
carl <xinfeng.zhao2@wework.com>
2019-10-17 10:42:02 +0800

    maximal square

--- src/main/java/com/leetcode/twohundredfifty/MaximalSquare.java
+++ src/main/java/com/leetcode/twohundredfifty/MaximalSquare.java
@@ -24,18 +24,10 @@ public class MaximalSquare {
         if (matrix.length == 0 || matrix[0].length == 0) return 0;
         int n = matrix.length, m = matrix[0].length;
         int ans = 0;
-        int[][] dp = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = matrix[i][0] - '0';
-            ans = Math.max(ans, dp[i][0]);
-        }
-        for (int i = 0; i < m; i++) {
-            dp[0][i] = matrix[0][i] - '0';
-            ans = Math.max(ans, dp[0][i]);
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][j] == '1') {
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
                     dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1])) + 1;
                     ans = Math.max(ans, dp[i][j]);
                 }
