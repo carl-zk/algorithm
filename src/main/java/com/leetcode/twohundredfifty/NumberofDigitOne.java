5236bbb878ef7d8bb574be841ec7aa65099bf811
carl <xinfeng.zhao2@wework.com>
2019-10-21 13:54:59 +0800

    todo

---
+++ src/main/java/com/leetcode/twohundredfifty/NumberofDigitOne.java
@@ -0,0 +1,15 @@
+package com.leetcode.twohundredfifty;
+
+/**
+ * @auther carl
+ */
+public class NumberofDigitOne {
+
+    public int countDigitOne(int n) {
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+
+        }
+        return ans;
+    }
+}
