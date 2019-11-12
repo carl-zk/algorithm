e4d84afa6893d3cf92ef8088fea83c502379daa1
Carl <zxfspace@163.com>
2019-11-12 23:10:03 +0800

    328. Odd Even Linked List

---
+++ src/main/java/com/leetcode/ListNode.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+/**
+ * @author carl
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
