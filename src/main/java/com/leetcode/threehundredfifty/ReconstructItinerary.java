e21ed990b329ff3d306bb53cbf88c031fd704df6
Carl <zxfspace@163.com>
2019-11-13 19:47:30 +0800

    332. Reconstruct Itinerary (欧拉回路)

--- src/main/java/com/leetcode/threehundredfifty/ReconstructItinerary.java
+++ src/main/java/com/leetcode/threehundredfifty/ReconstructItinerary.java
@@ -1,41 +1,50 @@
 package com.leetcode.threehundredfifty;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 /**
+ * https://leetcode.com/problems/reconstruct-itinerary/
+ * <p>
+ * Given a list of airline tickets represented by pairs of departure and arrival airports [from, to], reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK. Thus, the itinerary must begin with JFK.
+ * <p>
+ * Note:
+ * <p>
+ * If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string. For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+ * All airports are represented by three capital letters (IATA code).
+ * You may assume all tickets form at least one valid itinerary.
+ * Example 1:
+ * <p>
+ * Input: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+ * Output: ["JFK", "MUC", "LHR", "SFO", "SJC"]
+ * Example 2:
+ * <p>
+ * Input: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+ * Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+ * Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"].
+ * But it is larger in lexical order.
+ *
  * @auther carl
  */
 public class ReconstructItinerary {
+    Map<String, PriorityQueue<String>> graph = new HashMap<>();
+    List<String> ans = new LinkedList<>();
 
     public List<String> findItinerary(List<List<String>> tickets) {
-        Map<String, PriorityQueue<String>> map = new HashMap<>();
-        List<String> ans = new ArrayList<>();
-        for (List<String> pair : tickets) {
-            PriorityQueue<String> que = map.getOrDefault(pair.get(0), new PriorityQueue<>());
-            que.add(pair.get(1));
-            map.put(pair.get(0), que);
+        for (List<String> ticket : tickets) {
+            graph.computeIfAbsent(ticket.get(0), k -> new PriorityQueue<>()).add(ticket.get(1));
         }
-        int n = tickets.size() + 1;
-        solve(ans, map, "JFK", n);
+        eulerianPath("JFK");
         return ans;
     }
 
-    private void solve(List<String> ans, Map<String, PriorityQueue<String>> map, String start, int n) {
-        ans.add(start);
-        if (ans.size() == n) return;
-        PriorityQueue<String> que = map.get(start);
-        if (que == null || que.isEmpty()) {
-            ans.remove(ans.size() - 1);
-            return;
+    private void eulerianPath(String from) {
+        while (graph.containsKey(from) && !graph.get(from).isEmpty()) {
+            eulerianPath(graph.get(from).poll());
         }
-        PriorityQueue<String> copy = new PriorityQueue<>(que);
-
-        for (String next : copy) {
-            que.remove(next);
-            solve(ans, map, next, n);
-            if (ans.size() == n) return;
-            que.add(next);
-        }
-        ans.remove(ans.size() - 1);
+        ans.add(0, from);
     }
 }
