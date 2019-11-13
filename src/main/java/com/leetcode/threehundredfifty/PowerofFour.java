a6481ad94d4262c5f00ce6667775cf1b1f3c7202
Carl <zxfspace@163.com>
2019-11-13 21:09:35 +0800

    342. Power of Four

--- src/main/java/com/leetcode/threehundredfifty/PowerofFour.java
+++ src/main/java/com/leetcode/threehundredfifty/PowerofFour.java
@@ -20,11 +20,6 @@ package com.leetcode.threehundredfifty;
 public class PowerofFour {
 
     public boolean isPowerOfFour(int num) {
-        int n = 1;
-        for (int i = 0; i < 32 && n <= num; i += 2) {
-            if (n == num) return true;
-            n <<= 2;
-        }
-        return false;
+        return num > 0 && (num & (num - 1)) == 0 && (num & 0x55555555) != 0;
     }
 }
