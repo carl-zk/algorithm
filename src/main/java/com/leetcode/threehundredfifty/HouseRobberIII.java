a34347e0e60b321c8a1b9aeb3cd33f92d1550673
carl <xinfeng.zhao2@wework.com>
2019-11-14 17:00:52 +0800

    337. House Robber III

---
+++ src/main/java/com/leetcode/threehundredfifty/HouseRobberIII.java
@@ -0,0 +1,48 @@
+package com.leetcode.threehundredfifty;
+
+import com.leetcode.TreeNode;
+
+/**
+ * https://leetcode.com/problems/house-robber-iii/
+ * <p>
+ * The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+ * <p>
+ * Determine the maximum amount of money the thief can rob tonight without alerting the police.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [3,2,3,null,3,null,1]
+ * <p>
+ * 3
+ * / \
+ * 2   3
+ * \   \
+ * 3   1
+ * <p>
+ * Output: 7
+ * Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+ * Example 2:
+ * <p>
+ * Input: [3,4,5,1,3,null,1]
+ * <p>
+ * 3
+ * / \
+ * 4   5
+ * / \   \
+ * 1   3   1
+ * <p>
+ * Output: 9
+ * Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+ *
+ * @auther carl
+ */
+public class HouseRobberIII {
+
+    public int rob(TreeNode root) {
+        if (root == null) return 0;
+        int in = root.val + (root.left == null ? 0 : rob(root.left.left) + rob(root.left.right)) +
+                (root.right == null ? 0 : rob(root.right.left) + rob(root.right.right));
+        int ex = rob(root.left) + rob(root.right);
+        return Math.max(in, ex);
+    }
+}
