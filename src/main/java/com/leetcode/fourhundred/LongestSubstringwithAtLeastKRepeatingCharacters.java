2fde4be2e03238f850caec30ffd5c6db4a3bba2b
Carl <zxfspace@163.com>
2019-11-21 09:30:42 +0800

    395. Longest Substring with At Least K Repeating Characters

--- src/main/java/com/leetcode/fourhundred/LongestSubstringwithAtLeastKRepeatingCharacters.java
+++ src/main/java/com/leetcode/fourhundred/LongestSubstringwithAtLeastKRepeatingCharacters.java
@@ -1,6 +1,29 @@
 package com.leetcode.fourhundred;
 
 /**
+ * https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/
+ * <p>
+ * Find the length of the longest substring T of a given string (consists of lowercase letters only) such that every character in T appears no less than k times.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * s = "aaabb", k = 3
+ * <p>
+ * Output:
+ * 3
+ * <p>
+ * The longest substring is "aaa", as 'a' is repeated 3 times.
+ * Example 2:
+ * <p>
+ * Input:
+ * s = "ababbc", k = 2
+ * <p>
+ * Output:
+ * 5
+ * <p>
+ * The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
+ *
  * @author carl
  */
 public class LongestSubstringwithAtLeastKRepeatingCharacters {
@@ -15,8 +38,8 @@ public class LongestSubstringwithAtLeastKRepeatingCharacters {
         if (start > end) return;
         if (end - start < ans) return;
         int[] counter = new int[256];
-        for (char c : chars) {
-            counter[c]++;
+        for (int i = start; i <= end; i++) {
+            counter[chars[i]]++;
         }
         int i, j;
         for (i = start, j = start; i <= end; i++) {
@@ -30,5 +53,10 @@ public class LongestSubstringwithAtLeastKRepeatingCharacters {
         } else {
             solve(chars, j, i - 1, k);
         }
+    }
+
+    public static void main(String[] args) {
+        LongestSubstringwithAtLeastKRepeatingCharacters ls = new LongestSubstringwithAtLeastKRepeatingCharacters();
+        System.out.println(ls.longestSubstring("ababacb", 3));
     }
 }
