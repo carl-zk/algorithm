30b2ea26f22980cdfe077db55f6cd428ded86d2a
Carl <zxfspace@163.com>
2019-12-06 17:33:26 +0800

    441. Arranging Coins

---
+++ src/main/java/com/leetcode/fourhundredfifty/ArrangingCoins.java
@@ -0,0 +1,43 @@
+package com.leetcode.fourhundredfifty;
+
+/**
+ * https://leetcode.com/problems/arranging-coins/
+ * <p>
+ * You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+ * <p>
+ * Given n, find the total number of full staircase rows that can be formed.
+ * <p>
+ * n is a non-negative integer and fits within the range of a 32-bit signed integer.
+ * <p>
+ * Example 1:
+ * <p>
+ * n = 5
+ * <p>
+ * The coins can form the following rows:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤
+ * <p>
+ * Because the 3rd row is incomplete, we return 2.
+ * Example 2:
+ * <p>
+ * n = 8
+ * <p>
+ * The coins can form the following rows:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤ ¤
+ * ¤ ¤
+ * <p>
+ * Because the 4th row is incomplete, we return 3.
+ *
+ * @author carl
+ */
+public class ArrangingCoins {
+
+    public int arrangeCoins(int n) {
+        double t = n;
+        int ans = (int) Math.floor(Math.sqrt(t * 2 + 0.25) - 0.5);
+        return ans;
+    }
+}
