f4466079f231401d903a3fabea89665203545e68
Carl <zxfspace@163.com>
2019-12-10 09:43:10 +0800

    463. Island Perimeter

--- src/main/java/com/leetcode/fivehundred/IslandPerimeter.java
+++ src/main/java/com/leetcode/fivehundred/IslandPerimeter.java
@@ -24,47 +24,19 @@ package com.leetcode.fivehundred;
  * @author carl
  */
 public class IslandPerimeter {
-    int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    int M, N;
-    int perimeter = 0;
-
     public int islandPerimeter(int[][] grid) {
+        int M, N;
         if ((M = grid.length) == 0 || (N = grid[0].length) == 0) return 0;
-        int x = -1, y = 0;
-        for (int i = 0; i < M && x == -1; i++) {
+        int ones = 0, adjoins = 0;
+        for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
                 if (grid[i][j] == 1) {
-                    x = i;
-                    y = j;
-                    break;
+                    ones++;
+                    if (i + 1 < M && grid[i + 1][j] == 1) adjoins++;
+                    if (j + 1 < N && grid[i][j + 1] == 1) adjoins++;
                 }
             }
         }
-        if (x == -1) return 0;
-        dfs(grid, x, y);
-        return perimeter;
-    }
-
-    private void dfs(int[][] grid, int x, int y) {
-        perimeter += calPerimeter(grid, x, y);
-        grid[x][y] = -1;
-        for (int[] d : dirs) {
-            int i = x + d[0], j = y + d[1];
-            if (i < 0 || i >= M || j < 0 || j >= N || grid[i][j] == 0 || grid[i][j] == -1) {
-                continue;
-            }
-            dfs(grid, i, j);
-        }
-    }
-
-    private int calPerimeter(int[][] grid, int x, int y) {
-        int res = 0;
-        for (int[] d : dirs) {
-            int i = x + d[0], j = y + d[1];
-            if (i < 0 || i >= M || j < 0 || j >= N || grid[i][j] == 0) {
-                res += 1;
-            }
-        }
-        return res;
+        return ones * 4 - adjoins * 2;
     }
 }
